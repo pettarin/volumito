e8e3466e99945c7ed28331ee258b7013c4293cbd
Alberto Pettarin <pettarin@gmail.com>
2026-07-26 09:53:11 +0200

    Bump version to 0.0.15

--- src/volumito/__init__.py
+++ src/volumito/__init__.py
@@ -13,7 +13,7 @@ from volumito.clients import (
     VolumioRESTAPIClient,
 )
 
-__version__ = "0.0.14"
+__version__ = "0.0.15"
 __author__ = "Alberto Pettarin"
 __email__ = "alberto@albertopettarin.it"
 
